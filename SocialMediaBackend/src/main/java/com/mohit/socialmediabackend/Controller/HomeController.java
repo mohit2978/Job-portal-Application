ad2406f1accf060a3bf31ceb66f9fd84c4c710ca
Mohit Kumar <mohitrakheja007@gmail.com>
2025-08-30 23:32:26 +0530

    backend springboot initail

---
+++ SocialMediaBackend/src/main/java/com/mohit/socialmediabackend/Controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.mohit.socialmediabackend.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeControllerHandler() {
+
+        return "welcome to instagram backend api";
+
+    }
+
+}
