6faaa8f2e3ae927755840af04e5a2e5a3ee21d8e
mohit <mohitrakheja007@gmail.com>
2026-06-14 01:41:20 +0530

    JOb service created

---
+++ Backend/services/job-portal-job-service/src/main/java/com/mohit/job/JobPortalJobServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mohit.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalJobServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JobPortalJobServiceApplication.class, args);
+    }
+
+}
