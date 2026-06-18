88eca411728ed524f603ef5dceaa5a7e2eed843e
mohit <mohitrakheja007@gmail.com>
2026-06-18 21:23:31 +0530

    changed postgres to mysql

--- Backend/services/job-portal-user-service/src/main/java/com/mohit/job/security/JwtConstant.java
+++ Backend/services/job-portal-user-service/src/main/java/com/mohit/job/security/JwtConstant.java
@@ -1,5 +1,5 @@
 package com.mohit.job.security;
 
 public class JwtConstant {
-    public static String JWT_SECRET="secret";
+    public static String JWT_SECRET="secret5555%555558y3hjh4uujc0jkcj4koc4ckn4c4jbtch4vtchvcvbj45c5jb5c";
 }
